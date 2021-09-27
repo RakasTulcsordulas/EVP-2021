ad4199e770188aafd97a65cd14e9cf3ba23b7e55
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-09-27 09:07:15 +0200

    Intellij demo + required dlls

---
+++ src/main/java/com/app/evp2021/Main.java
@@ -0,0 +1,7 @@
+package com.app.evp2021;
+
+public class Main {
+    public static void main(String[] args){
+        HelloApplication.main(args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.app.evp2021 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.app.evp2021 to javafx.fxml;
+    exports com.app.evp2021;
+}
