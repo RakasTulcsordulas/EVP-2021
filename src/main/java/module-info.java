9f5acb523d398b4d8921e35f1ffb5c9db905fe98
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-11-02 12:37:01 +0100

    Files changed automatically

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,4 +7,8 @@ module com.app.evp2021 {
 
     opens com.app.evp2021 to javafx.fxml;
     exports com.app.evp2021;
+    exports com.app.evp2021.controllers;
+    opens com.app.evp2021.controllers to javafx.fxml;
+    exports com.app.evp2021.views;
+    opens com.app.evp2021.views to javafx.fxml;
 }
