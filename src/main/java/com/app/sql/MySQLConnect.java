c9766881f41fcf256765e49baef23e85eb684e64
t1kjui <t1kjui@inf.elte.hu>
2021-11-04 16:21:24 +0100

    Added more basic SQL handling methods and get method parameters can be optional now.

--- src/main/java/com/app/sql/MySQLConnect.java
+++ src/main/java/com/app/sql/MySQLConnect.java
@@ -70,9 +70,8 @@ public class MySQLConnect {
      * Executes an INSERT INTO command into 'employee' table on a given connection. Passwords are hashed on server side, salt is a randomized 10 character string.
      * @param username varchar(30)
      * @param password varchar(224)
-     * @param emailAddress varchar(50)
      */
-    public void signUpNewEmployee (String username, String password, String emailAddress) {
+    public void signUpNewEmployee (String username, String password) {
 
         String query = "INSERT INTO employee (id, username, password, salt) VALUES (NULL, ?, ?, ?)";
 
@@ -89,7 +88,6 @@ public class MySQLConnect {
             preparedStatement.setString(3, salt);
 
             int result = preparedStatement.executeUpdate();
-            System.out.println(result);
             if (result > 0) {
                 System.out.println("New employee is inserted into the database!");
             } else {
@@ -219,16 +217,29 @@ public class MySQLConnect {
 
     /**
      * Executes an SQL query on the 'movie' table with the given title.
-     * @param title Title to be searched in the database, case-sensitive.
-     * @return an array of object starting from 1, containing the records of the movie the order as stored in the database.
+     * @param id Id of the movie. Can be null
+     * @param title Title to be searched in the database, case-sensitive. Can be null
+     * @param duration_min duration of the movie in minutes int(11). Can be null
+     * @return an array of objects starting from 1, containing the records of the movie in the order as stored in the database.
      */
-    public Object[] getMovie(String title) {
+    public Object[] getMovie(Object id, Object title, Object duration_min) {
         Object[] resultSetObject = null;
-        String query = "SELECT * FROM movie WHERE title = ?";
+        String query = "SELECT * FROM movie WHERE " +
+                "(? IS NULL OR ? = id)" +
+                "AND " +
+                "(? IS NULL OR ? = title)" +
+                "AND " +
+                "(? IS NULL OR ? = duration_min)";
 
         System.out.println("Searching for movie...");
         try (PreparedStatement preparedStatement = con.prepareStatement(query)){
-            preparedStatement.setString(1, title);
+            preparedStatement.setObject(1, id);
+            preparedStatement.setObject(2, id);
+            preparedStatement.setObject(3, title);
+            preparedStatement.setObject(4, title);
+            preparedStatement.setObject(5, duration_min);
+            preparedStatement.setObject(6, duration_min);
+
             ResultSet resultSet = preparedStatement.executeQuery();
 
             if (resultSet.next()) {
@@ -238,10 +249,131 @@ public class MySQLConnect {
                 for (int i = 1; i < rsmd.getColumnCount(); ++i) {
                     resultSetObject[i] = resultSet.getObject(i);
                 }
-            } else {return null;}
+            } else {
+                System.out.println("No movie is found.");
+                return null;}
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        return resultSetObject;
+    }
+
+    //*************************
+    //** AUDITORIUM HANDLING **
+    //*************************
+
+    /**
+     * Executes and INSERT INTO update on the auditorium table with the given parameters.
+     * @param name name of the auditorium varchar(32)
+     * @param seats_no total number of seats in the auditorium int(11)
+     */
+    public void insertAuditorium (String name, int seats_no) {
+        String query = "INSERT INTO auditorium (id, name, seats_no) VALUES (NULL, ?, ?)";
+
+        System.out.println("Inserting new auditorium...");
+        try (PreparedStatement preparedStatement = con.prepareStatement(query)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setInt(2, seats_no);
+
+            if (preparedStatement.executeUpdate() > 0) {
+                System.out.println("New auditorium " + name + " is inserted into the database.");
+            } else {
+                System.out.println("Could not insert " + name + "into the database.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Executes an INSERT INTO update on the seat table with the given parameters.
+     * @param row Row of the seat int(11).
+     * @param number Number of the seat int(11).
+     * @param auditorium_id Id of the auditorium containing the seat FK of auditorium int(11).
+     */
+    public void insertSeat (int row, int number, int auditorium_id) {
+        String query = "INSERT INTO seat (id, row, number, auditorium_id) VALUES (NULL, ?, ?, ?)";
+
+        try (PreparedStatement preparedStatement = con.prepareStatement(query)) {
+            preparedStatement.setInt(1, row);
+            preparedStatement.setInt(2, number);
+            preparedStatement.setInt(3, auditorium_id);
+
+            if (preparedStatement.executeUpdate() > 0) {
+                System.out.println("New seat inserted");
+            } else {
+                System.out.println("Error! Insertion failed!");
+            }
+        } catch (SQLException e) {e.printStackTrace();}
+    }
+
+    //************************
+    //** SCREENING HANDLING **
+    //************************
+
+    /**
+     * Executes an INSERT INTO update on the screening table with the given parameters.
+     * @param movie_id Id of the inserted movie FK of movie int(11).
+     * @param auditorium_id Id of the auditorium having the screening FK of auditorium int(11).
+     * @param screening_start Timestamp of the screening as [YYYY-MM-DD hh:mm:ss]
+     */
+    public void insertScreening (int movie_id, int auditorium_id, Timestamp screening_start) {
+        String query = "INSERT INTO screening (id, movie_id, auditorium_id, screening_start) VALUES (NULL, ?, ?, ?)";
+
+        try (PreparedStatement preparedStatement = con.prepareStatement(query)){
+            preparedStatement.setInt(1, movie_id);
+            preparedStatement.setInt(2, auditorium_id);
+            preparedStatement.setTimestamp(3, screening_start);
+
+            if (preparedStatement.executeUpdate() > 0) {
+                System.out.println("New screening inserted.");
+            } else {
+                System.out.println("Error! Insertion failed!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Executes a SELECT query on the screening table with any combination of the given parameters.
+     * @param id Id of the screening AUTO INCREMENT int(11).
+     * @param movie_id Id of the inserted movie FK of movie int(11).
+     * @param auditorium_id Id of the auditorium having the screening FK of auditorium int(11).
+     * @param screening_start Timestamp of the screening as [YYYY-MM-DD hh:mm:ss]. Can be null
+     * @return An Object[] starting from 1 containing in sequence every data of all rows where at least one given parameter is met.
+     */
+    public Object[] getScreening (Object id, Object movie_id, Object auditorium_id, Timestamp screening_start) {
+        Object[] resultSetObject = null;
+        String query = "SELECT * FROM screening WHERE " +
+                "(? IS NULL OR ? = id) " +
+                "AND " +
+                "(? IS NULL OR ? = movie_id) " +
+                "AND " +
+                "(? IS NULL OR ? = auditorium_id) " +
+                "AND " +
+                "(? IS NULL OR ? = screening_start)";
+
+        try (PreparedStatement preparedStatement = con.prepareStatement(query)) {
+            preparedStatement.setObject(1, id);
+            preparedStatement.setObject(2, id);
+            preparedStatement.setObject(3, movie_id);
+            preparedStatement.setObject(4, movie_id);
+            preparedStatement.setObject(5, auditorium_id);
+            preparedStatement.setObject(6, auditorium_id);
+            preparedStatement.setTimestamp(7, screening_start);
+            preparedStatement.setTimestamp(8, screening_start);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                ResultSetMetaData rsmd = resultSet.getMetaData();
+                resultSetObject = new Object[rsmd.getColumnCount()];
+
+                for (int i = 1; i < rsmd.getColumnCount(); ++i) {
+                    resultSetObject[i] = resultSet.getObject(i);
+                }
+            } else {return null;}
+        } catch (SQLException e) {e.printStackTrace();}
         return resultSetObject;
     }
 
