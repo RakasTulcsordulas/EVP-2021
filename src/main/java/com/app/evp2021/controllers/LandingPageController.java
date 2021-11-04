2c3a4e49fa317d4d8ef26d062f2c163ca1f09851
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-11-04 21:08:04 +0100

    LandingPage with controller and fxml file
    Basic initialization, finished UI and Application class, controller contains neccessary functions.

--- src/main/java/com/app/evp2021/controllers/LandingPageController.java
+++ src/main/java/com/app/evp2021/controllers/LandingPageController.java
@@ -1,8 +1,48 @@
 package com.app.evp2021.controllers;
 
+import com.app.evp2021.Main;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import com.app.sql.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 
-public class LandingPageController {
+import java.net.URL;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.ResourceBundle;
 
+public class LandingPageController{
+
+
+    @FXML private Text current_date;
+    @FXML private DatePicker date_picker;
+    @FXML private ScrollPane movie_scroll;
+    @FXML private void onActionPerformed(ActionEvent event) {
+        current_date.setText("Kiválasztott dátum: " + date_picker.getValue().toString());
+    }
+
+    @FXML private void listMovies() {
+        MySQLConnect con = new MySQLConnect();
+        con.establishConnection();
+        String time = date_picker.getValue().toString() + " 12:00:00";
+        Timestamp ts = Timestamp.valueOf("2021-11-04 20:09:23");
+        System.out.println(ts);
+        Object[] res = con.getScreening(null,null,null,ts);
+        System.out.println(res);
+    }
+
+    public void initialize() {
+        date_picker.setValue(LocalDate.now());
+        current_date.setText("Kiválasztott dátum: " + date_picker.getValue().toString());
+    }
 }
--- src/main/java/com/app/evp2021/views/LandingPage.java
+++ src/main/java/com/app/evp2021/views/LandingPage.java
@@ -1,11 +1,21 @@
 package com.app.evp2021.views;
 
 import com.app.evp2021.Main;
+import com.app.evp2021.controllers.LandingPageController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.skin.DatePickerSkin;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class LandingPage extends Application {
 
@@ -15,12 +25,27 @@ public class LandingPage extends Application {
 
     @Override
     public void start(Stage stage) throws Exception{
-        Parent root = FXMLLoader.load(Main.class.getResource("landing-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("landing-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("CinemApp - Főoldal");
-        stage.setScene(new Scene(root));
-        stage.minWidthProperty().setValue(640);
-        stage.minHeightProperty().setValue(480);
+
+        stage.setMinWidth(1024);
+        stage.setMinHeight(768);
+
         stage.setMaximized(true);
+
+        DatePicker dp = (DatePicker) scene.lookup("#datepicker");
+        DatePickerSkin dps = new DatePickerSkin(dp);
+        HBox holder = (HBox) scene.lookup("#dateholder");
+        holder.getChildren().add(dps.getPopupContent());
+        holder.getChildren().remove(dp);
+
+        scene.getStylesheets().add(Main.class.getResource("css/bootstrap.css").toExternalForm());
+        scene.getStylesheets().add(Main.class.getResource("css/main.css").toExternalForm());
+
+        stage.setScene(scene);
         stage.show();
+
+
     }
 }
