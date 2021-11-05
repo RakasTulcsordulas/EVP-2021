8b70dc3670a237df1628b1695bb89f662a4be1d2
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-11-05 12:28:31 +0100

    Landing page cleaning, added logged in and logged out scene

--- src/main/java/com/app/evp2021/views/LandingPage.java
+++ src/main/java/com/app/evp2021/views/LandingPage.java
@@ -2,6 +2,7 @@ package com.app.evp2021.views;
 
 import com.app.evp2021.Main;
 import com.app.evp2021.controllers.LandingPageController;
+import com.app.evp2021.services.UserSession;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -11,6 +12,7 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.skin.DatePickerSkin;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -22,30 +24,54 @@ public class LandingPage extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+    private static Stage _stage = null;
 
     @Override
     public void start(Stage stage) throws Exception{
-        stage.setMinWidth(1024);
-        stage.setMinHeight(768);
+        _stage = stage;
 
-        stage.setMaximized(true);
+        Scene sc = getLoggedOutScene();
 
-        Scene sc = getLoggedOfScene(stage);
-
-        stage.setScene(sc);
-        stage.show();
+        showStage(sc);
     }
 
-    public Scene getLoggedOfScene(Stage stage) throws Exception{
+    public static void showStage (Scene sc) {
+        _stage.close();
+        _stage.setMinWidth(1024);
+        _stage.setMinHeight(768);
+
+        _stage.setMaximized(true);
+        _stage.setScene(sc);
+        _stage.show();
+    }
+
+    public static void refresh() throws Exception{
+        boolean logged = UserSession.getSession().getLoggedIn();
+        if(logged == true) { showStage(getLoggedInScene()); }else{ showStage(getLoggedOutScene());}
+    }
+
+    public static Scene getLoggedOutScene() throws Exception{
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("landing-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("CinemApp - Főoldal");
+        _stage.setTitle("CinemApp - Főoldal");
 
         DatePicker dp = (DatePicker) scene.lookup("#datepicker");
         DatePickerSkin dps = new DatePickerSkin(dp);
         HBox holder = (HBox) scene.lookup("#dateholder");
         holder.getChildren().add(dps.getPopupContent());
         holder.getChildren().remove(dp);
+
+        scene.getStylesheets().add(Main.class.getResource("css/bootstrap.css").toExternalForm());
+        scene.getStylesheets().add(Main.class.getResource("css/main.css").toExternalForm());
+
+        return scene;
+    }
+
+    public static Scene getLoggedInScene() throws Exception{
+        System.out.println("meghivva");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("admin-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        _stage.setTitle("CinemApp - Admin");
 
         scene.getStylesheets().add(Main.class.getResource("css/bootstrap.css").toExternalForm());
         scene.getStylesheets().add(Main.class.getResource("css/main.css").toExternalForm());
