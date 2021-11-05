bceec495c5b1eb4e097e5f979d09087f3de782b4
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-11-05 09:53:48 +0100

    Dummy hardcoded data added to test overflow and movie listing

--- src/main/java/com/app/evp2021/controllers/LandingPageController.java
+++ src/main/java/com/app/evp2021/controllers/LandingPageController.java
@@ -1,6 +1,7 @@
 package com.app.evp2021.controllers;
 
 import com.app.evp2021.Main;
+import com.app.evp2021.views.LoginModal;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -8,18 +9,14 @@ import javafx.scene.control.*;
 import com.app.sql.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class LandingPageController{
 
@@ -27,22 +24,52 @@ public class LandingPageController{
     @FXML private Text current_date;
     @FXML private DatePicker date_picker;
     @FXML private ScrollPane movie_scroll;
+    @FXML private VBox movie_list;
     @FXML private void onActionPerformed(ActionEvent event) {
         current_date.setText("Kiválasztott dátum: " + date_picker.getValue().toString());
     }
 
     @FXML private void listMovies() {
-        MySQLConnect con = new MySQLConnect();
+        /*MySQLConnect con = new MySQLConnect();
         con.establishConnection();
         String time = date_picker.getValue().toString() + " 12:00:00";
-        Timestamp ts = Timestamp.valueOf("2021-11-04 20:09:23");
-        System.out.println(ts);
-        Object[] res = con.getScreening(null,null,null,ts);
-        System.out.println(res);
+        Timestamp ts = Timestamp.valueOf("2021-11-04 20:09:23");*/
+        movie_list.getChildren().clear();
+        String[][] res = {{"Elkurtuk", "Terem1", "2021-11-04 11:09:23"}, {"Dune", "Terem2", "2021-11-04 20:09:23"}, {"Elkurtuk", "Terem1", "2021-11-04 15:09:23"}};
+        for(int i=0; i< res.length; i++){
+            Button b = new Button(res[i][0] + " | " + res[i][1] + " | " + res[i][2].substring(11,res[i][2].length()));
+            b.getStyleClass().add("btn-primary");
+            b.setMinWidth(movie_scroll.getWidth()-25);
+            movie_list.getChildren().add(b);
+        }
+        for(int i=0; i< res.length; i++){
+            Button b = new Button(res[i][0] + " | " + res[i][1] + " | " + res[i][2].substring(11,res[i][2].length()));
+            b.getStyleClass().add("btn-primary");
+            b.setMinWidth(movie_scroll.getWidth()-25);
+            movie_list.getChildren().add(b);
+        }
+        for(int i=0; i< res.length; i++){
+            Button b = new Button(res[i][0] + " | " + res[i][1] + " | " + res[i][2].substring(11,res[i][2].length()));
+            b.getStyleClass().add("btn-primary");
+            b.setMinWidth(movie_scroll.getWidth()-25);
+            movie_list.getChildren().add(b);
+        }
+    }
+
+    @FXML
+    private void openAdminModal() {
+        System.out.println("nyom");
+        try {
+            LoginModal.display();
+        }catch (Exception e){
+
+        }
+
     }
 
     public void initialize() {
         date_picker.setValue(LocalDate.now());
         current_date.setText("Kiválasztott dátum: " + date_picker.getValue().toString());
+        movie_list.setSpacing(20);
     }
 }
