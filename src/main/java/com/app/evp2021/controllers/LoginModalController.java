08cd9151c04a39779b69e5b69e7f6d6c40230baf
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-11-05 12:27:33 +0100

    Added LoginModalController

--- src/main/java/com/app/evp2021/controllers/LoginModalController.java
+++ src/main/java/com/app/evp2021/controllers/LoginModalController.java
@@ -1,4 +1,38 @@
 package com.app.evp2021.controllers;
 
+import com.app.evp2021.services.UserSession;
+import com.app.evp2021.views.LandingPage;
+import com.app.evp2021.views.LoginModal;
+import com.app.sql.MySQLConnect;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
 public class LoginModalController {
+    @FXML private TextField username;
+    @FXML private PasswordField psw;
+    @FXML private Text errormsg;
+
+    @FXML
+    private void onLoginAttempt() throws Exception{
+        if(username.getText().isEmpty() || psw.getText().isEmpty()) {
+            errormsg.setText("Minden mező kitöltése kötelező!");
+        }else{
+
+            MySQLConnect con = new MySQLConnect();
+            con.establishConnection();
+            boolean login = con.checkPassword(username.getText(), psw.getText());
+
+            if(login) {
+                LoginModal.showSuccessPane("Sikeres belépés! Üdvözöllek " + username.getText());
+                UserSession.setSession(username.getText(), true);
+                LandingPage.refresh();
+            }else{
+                errormsg.setText("Autentikációs hiba történt!");
+            }
+
+        }
+    }
 }
