723a5270da94b37de94f788610cff3886a17436b
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-11-18 20:20:23 +0100

    Auditorium UI generator, stash caused problems deleted.

--- src/main/java/com/app/evp2021/controllers/Auditorium.java
+++ src/main/java/com/app/evp2021/controllers/Auditorium.java
@@ -17,63 +17,43 @@ import javafx.scene.text.Text;
 import java.io.IOException;
 import java.util.Collection;
 
-<<<<<<< Updated upstream
-public class Auditorium extends AnchorPane{
-=======
+
 public class Auditorium {
->>>>>>> Stashed changes
+
 
     @FXML private AnchorPane seat_holder;
     @FXML private Text movie_title;
 
-<<<<<<< Updated upstream
-    private static GridPane root = null;
-    private String title = null;
 
-=======
     @FXML private GridPane root = null;
     @FXML private AnchorPane auditorium;
     private String title = null;
 
-    public Auditorium() {
-        System.out.println("constr");
-    }
-
->>>>>>> Stashed changes
-    public void create(int _col, int _row) {
+    public void create() {
         root = new GridPane();
         root.setHgap(12);
         root.setVgap(12);
-<<<<<<< Updated upstream
-        final int size_row = _row;
-        final int size_col = _col;
-=======
+
+
         final int size_row = 18;
         final int size_col = 19;
->>>>>>> Stashed changes
-        final String[] letters = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r"};
+
         for (int row = 0; row < size_row; row++) {
             for (int col = 0; col < size_col; col ++) {
                 Label text = new Label();
                 text.setWrapText(true);
-<<<<<<< Updated upstream
-                text.setText(col+1 + letters[row]);
-                text.getStyleClass().add("seat");
-=======
 
->>>>>>> Stashed changes
+                text.getStyleClass().add("seat");
+
                 StackPane p = new StackPane();
                 p.getChildren().add(text);
                 StackPane.setAlignment(text, Pos.CENTER);
                 p.setMaxHeight(50);
                 p.setMaxWidth(50);
-<<<<<<< Updated upstream
-                p.getStyleClass().add("btn-danger");
-=======
-                p.setId("" + ((row-1)*19)+col);
 
                 if(col == 0) {
                     text.setText(""+(row+1));
+                    text.setStyle("-fx-color: black");
                 }else{
                     if(col <= 9) {
                         text.setText((9-col)+1 + "b");
@@ -81,9 +61,9 @@ public class Auditorium {
                         text.setText(col-9 + "j");
                     }
                     text.getStyleClass().add("seat");
+                    p.setVisible(false);
                 }
-                p.setVisible(false);
->>>>>>> Stashed changes
+
                 root.add(p, col, row);
             }
         }
@@ -100,14 +80,12 @@ public class Auditorium {
         seat_holder.setRightAnchor(root, 0.0);
         seat_holder.setTopAnchor(root, 0.0);
         seat_holder.setBottomAnchor(root, 0.0);
-<<<<<<< Updated upstream
+
 
 
         int ti = size_row * size_col;
         title = String.valueOf(ti);
         movie_title.setText(title);
-    }
-=======
     }
 
     public void setTitle(String s){
@@ -116,7 +94,8 @@ public class Auditorium {
 
     public void setSeat(int row, int col, int status) throws Exception{
         if(row <= 0 || col <= 0) throw new Exception("0-nál nagyobb szám elvárt!");
-        StackPane e = (StackPane) root.lookup("#" + ((row-1)*19)+col);
+        int index = ((row-1)*19)+col;
+        StackPane e = (StackPane) root.getChildren().get(index);
         switch (status){
             case -1:
                 e.setVisible(false);
@@ -138,7 +117,4 @@ public class Auditorium {
     public void showSelf(boolean b){
         auditorium.setVisible(b);
     }
-
-
->>>>>>> Stashed changes
 }
