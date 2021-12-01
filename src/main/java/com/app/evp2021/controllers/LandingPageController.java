13fd241def05fee096a9ab8540ff04ac901c4327
Gömböcz Zsolt <zsolt.gombocz00gmail.com>
2021-12-01 15:56:04 +0100

    Replaced old input handling with MovieInputsController method calls

--- src/main/java/com/app/evp2021/controllers/LandingPageController.java
+++ src/main/java/com/app/evp2021/controllers/LandingPageController.java
@@ -1,6 +1,7 @@
 package com.app.evp2021.controllers;
 
 import com.app.evp2021.Main;
+import com.app.evp2021.services.MovieInputsController;
 import com.app.evp2021.services.UserSession;
 import com.app.evp2021.views.AuditoriumCreationPopup;
 import com.app.evp2021.views.LandingPage;
@@ -9,12 +10,10 @@ import com.app.sql.MySQLConnect;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.text.Text;
-import javafx.util.Duration;
 
 import java.sql.SQLException;
 
@@ -25,6 +24,7 @@ public class LandingPageController{
     @FXML private DatePicker date_picker;
     @FXML private ScrollPane movie_scroll;
     @FXML private Label once_text;
+    @FXML private CheckBox once_checkbox;
     @FXML private VBox movie_inputs;
     @FXML private Text input_title;
     @FXML private Button save_movie;
@@ -55,22 +55,9 @@ public class LandingPageController{
     private CheckBox[] screening_day_boxes;
     private ChoiceBox[] screening_day_times;
     private boolean new_movie;
+    private MovieInputsController movieInputsController = null;
 
-    private Tooltip setTooltip(Node node, String text) {
-        Tooltip tooltip = new Tooltip();
-        tooltip.setShowDelay(Duration.millis(100));
-        tooltip.setPrefWidth(300);
-        tooltip.setWrapText(true);
-        tooltip.setText(text);
-        tooltip.setStyle("-fx-font-size: 12px;");
-        Tooltip.install(node,tooltip);
 
-        return tooltip;
-    }
-
-    private void removeTooltip(Node node, Tooltip tooltip) {
-        Tooltip.uninstall(node, tooltip);
-    }
 
     @FXML private void onActionPerformed(ActionEvent event) {
         current_date.setText("Kiválasztott dátum: " + date_picker.getValue().toString());
@@ -126,9 +113,13 @@ public class LandingPageController{
 
     @FXML
     void addMovie(MouseEvent event) {
+        movieInputsController.addNewMovie();
         new_movie = true;
-        delete_movie.setDisable(true);
-        setUpInputs();
+    }
+
+    @FXML
+    void refreshAdminView(MouseEvent event) throws Exception {
+        LandingPage.refresh();
     }
 
     @FXML
@@ -188,208 +179,19 @@ public class LandingPageController{
 
     }
 
-    public void setUpInputs(Object movie_title, Object directors, Object cast, Object desc, Object rating, Object auditroom, Object[] screening, boolean one_screening) {
-        setTooltip(once_text, "Ez a funkció hasznos ha egy filmet csakis EGYSZER szeretnénk játszani, a jelőlő négyzeteknél egyet bepipálva adhatjuk meg mikor legyen játszva a film, ha már ay a nap elmúlt, a következő hátre fog csúszni. Ha több van bejelölve, akkor is csak egy időpont lesz kiválasztva.");
-        input_title.setText(movie_title + " című film szerkesztése");
-
-        setRatingsInDropdown();
-
-        movie_title_input.setText(movie_title.toString());
-        director_input.setText((directors != null) ? directors.toString() : "");
-        cast_input.setText((cast != null) ? cast.toString() : "");
-        description_input.setText((desc != null) ? desc.toString() : "");
-
-        setAuditoriumsInDropdown();
-    }
-
-    public void setUpInputs() {
-
-        setTooltip(once_text, "Ez a funkció hasznos ha egy filmet csakis EGYSZER szeretnénk játszani, a jelőlő négyzeteknél egyet bepipálva adhatjuk meg mikor legyen játszva a film, ha már ay a nap elmúlt, a következő hátre fog csúszni. Ha több van bejelölve, akkor is csak egy időpont lesz kiválasztva.");
-
-        setRatingsInDropdown();
-
-        movie_inputs.setVisible(true);
-        input_title.setText("Új film hozzáadása");
-
-        save_movie.setStyle("-fx-cursor: hand;");
-        delete_movie.setStyle("-fx-cursor: none;");
-
-        screening_day_boxes = new CheckBox[]{check_1, check_2, check_3, check_4, check_5, check_6, check_7};
-        screening_day_times = new ChoiceBox[]{choice_1, choice_2, choice_3, choice_4, choice_5, choice_6, choice_7};
-
-        setAuditoriumsInDropdown();
-    }
-
-    private void setAuditoriumsInDropdown(){
-        movie_room.getItems().clear();
-
-        MySQLConnect dbConnection = new MySQLConnect();
-        try {
-            dbConnection.establishConnection();
-        }catch (SQLException err){
-            err.printStackTrace();
-        }
-
-        Object[][] resultAuditoriums = dbConnection.getAuditorium(null);
-
-        dbConnection.closeConnection();
-
-        for(int i = 1; i < resultAuditoriums.length; i++) {
-            movie_room.getItems().add(resultAuditoriums[i][2]);
-        }
-    }
-
-    private void setRatingsInDropdown() {
-        rating_drop.getItems().clear();
-
-        rating_drop.getItems().add(6);
-        rating_drop.getItems().add(12);
-        rating_drop.getItems().add(16);
-        rating_drop.getItems().add(18);
-
-        rating_drop.setValue(6);
-    }
-
-    @FXML
-    void deleteMovie(MouseEvent event) {
-
-    }
-
     @FXML
     void closeInputs(MouseEvent event) {
-        resetInputs();
-        movie_inputs.setVisible(false);
+        movieInputsController.closeInputFields();
     }
 
-    public boolean checkInputs() {
-        boolean errorFound = true;
-        Tooltip t1 = null, t2 = null, t3 = null;
-
-        if(movie_title_input.getText().isEmpty()) {
-            movie_title_input.getStyleClass().add("input-error");
-            setErrorClass(movie_title_input);
-            t1 = setTooltip(movie_title_input, "Ezt a mezőt kötelező kitölteni!");
-            errorFound = false;
-        }else{
-           removeErrorClass(movie_title_input);
-            if(t1 != null){ removeTooltip(movie_title_input, t1); }
-        }
-
-        if(isScreeningSelected()){
-            if(movie_room.getValue() == null) {
-                setErrorClass(movie_room);
-                t2 = setTooltip(movie_room, "Ezt a mezőt kötelező kitölteni!");
-                errorFound = false;
-            }else{
-                removeErrorClass(movie_room);
-                if(t2 != null) { removeTooltip(movie_room, t2); }
-            }
-        }
 
 
-        if(duration_input.getText().isEmpty()) {
-            setErrorClass(duration_input);
-            t3 = setTooltip(duration_input, "Ezt a mezőt kötelező kitölteni!");
-            errorFound = false;
-        }else{
-            try{
-                int dur = Integer.parseInt(duration_input.getText());
-
-                if(dur < 10 || dur > 250) {
-                    setErrorClass(duration_input);
-                    t3 = setTooltip(duration_input, "10 és 250 között kell lennie a hossznak!");
-                    errorFound = false;
-                }else{
-
-                    removeErrorClass(duration_input);
-                    if(t3 != null) { removeTooltip(duration_input, t3); }
-                }
-
-            }catch (NumberFormatException err) {
-                t3 = setTooltip(duration_input, "Számot kell megadni film hossznak.");
-                setErrorClass(duration_input);
-                errorFound = false;
-            }
-        }
-
-        return errorFound;
-    }
-
-    private boolean isScreeningSelected() {
-        for(int i = 0; i < screening_day_boxes.length; i++) {
-            if(screening_day_boxes[i].isSelected()) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean isScreeningTimeSelected() {
-        for(int i = 0; i < screening_day_times.length; i++) {
-            if(screening_day_times[i].getValue() != null) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public void setErrorClass(Node node){
-        if(node instanceof TextField && !(node.getStyleClass().contains("input-error"))){
-            node.getStyleClass().add("input-error");
-        }else if((node instanceof ChoiceBox || node instanceof CheckBox ) && !(node.getStyleClass().contains("choice-error"))){
-            node.getStyleClass().add("choice-error");
-        }
-    }
-
-    public void removeErrorClass(Node node) {
-        if(node instanceof TextField && (node.getStyleClass().contains("input-error"))){
-            node.getStyleClass().remove("input-error");
-        }else if((node instanceof ChoiceBox || node instanceof CheckBox ) && (node.getStyleClass().contains("choice-error"))){
-            node.getStyleClass().remove("choice-error");
-        }
-    }
-
-    public boolean checkScreeningInput() {
-        boolean errorFound = true;
-        Tooltip t[] = new Tooltip[7];
-
-        for(int i = 0; i < 7; i++) {
-            if(isScreeningSelected() || isScreeningTimeSelected()){
-                if(screening_day_boxes[i].isSelected() && screening_day_times[i].getValue() == null){
-                    setErrorClass(screening_day_times[i]);
-                    t[i] = setTooltip(screening_day_times[i], "Adott naphoz kötelező megjelőlni kezdődátumot.");
-                    errorFound = false;
-                }else if(screening_day_times[i].getValue() != null && !screening_day_boxes[i].isSelected()) {
-                    setErrorClass(screening_day_times[i]);
-                    t[i] = setTooltip(screening_day_times[i], "Adott dátumhoz kötelező kiválasztani a napot is!");
-                    errorFound = false;
-                }
-            }else{
-                removeErrorClass(screening_day_boxes[i]);
-                removeTooltip(screening_day_boxes[i], t[i]);
-                removeErrorClass(screening_day_times[i]);
-                removeTooltip(screening_day_times[i], t[i]);
-            }
-        }
-
-        return errorFound;
-    }
-
-    private void resetInputs() {
-        movie_title_input.setText("");
-        director_input.setText("");
-        cast_input.setText("");
-        description_input.setText("");
-        duration_input.setText("");
-        movie_room.setValue(null);
-    }
     @FXML
     void saveMovie(MouseEvent event) throws Exception {
         if(new_movie){
-            boolean inputValid = checkInputs();
-            boolean boxesValid =  checkScreeningInput();
+            Object[] validationValues = movieInputsController.validateInputFields();
 
-            if(inputValid && boxesValid){
+            if(validationValues != null){
                 MySQLConnect dbConnection = new MySQLConnect();
                 try{
                     dbConnection.establishConnection();
@@ -398,16 +200,15 @@ public class LandingPageController{
                 }
 
                 dbConnection.insertNewMovie(
-                        movie_title_input.getText(),
-                        (director_input.getText().isEmpty()) ? null : director_input.getText(),
-                        (cast_input.getText().isEmpty()) ? null : cast_input.getText(),
-                        (description_input.getText().isEmpty()) ? null : description_input.getText(),
-                        rating_drop.getValue().toString(),
-                        duration_input.getText()
+                        (String) validationValues[0],
+                        (String) validationValues[1],
+                        (String) validationValues[2],
+                        (String) validationValues[3],
+                        (String) validationValues[4],
+                        (String) validationValues[6]
                 );
 
-                movie_inputs.setVisible(false);
-                resetInputs();
+                movieInputsController.closeInputFields();
                 LandingPage.refresh();
             }
         }else{
@@ -436,15 +237,10 @@ public class LandingPageController{
                 movie_holder.setSpacing(20.0);
 
                 final int rowIndex = i;
-                final Object[][] movie = resultMovies;
+                final Object[][] movies = resultMovies;
 
                 movieButton.setOnMouseClicked(event -> {
-                    try {
-                        movie_inputs.setVisible(true);
-                        setUpInputs(movie[rowIndex][2], movie[rowIndex][3], movie[rowIndex][4], movie[rowIndex][5], movie[rowIndex][6], movie[rowIndex][7],null, false);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+                    movieInputsController.editExistingMovie(movies[rowIndex]);
                 });
             }
         }catch (SQLException err){
@@ -482,6 +278,15 @@ public class LandingPageController{
             }
         }catch (SQLException err){
             err.printStackTrace();
+        }
+    }
+    //set up initial things
+    public void initializeAdminView() {
+        if(movieInputsController == null) {
+            screening_day_boxes = new CheckBox[]{check_1, check_2, check_3, check_4, check_5, check_6, check_7};
+            screening_day_times = new ChoiceBox[]{choice_1, choice_2, choice_3, choice_4, choice_5, choice_6, choice_7};
+            Object[] inputFields = {movie_title_input, director_input, cast_input, description_input, rating_drop, movie_room, duration_input, screening_day_boxes, screening_day_times};
+            movieInputsController = new MovieInputsController(movie_inputs, inputFields, input_title, save_movie, delete_movie, once_text, once_checkbox);
         }
     }
 }
